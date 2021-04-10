9c43a2609411ce0c73753bc4caf38d5b71e7ab91
Aakash03122005 <69099671+Aakash03122005@users.noreply.github.com>
2021-04-10 12:15:57 +0530

    Update ingredients.js

--- ingredients.js
+++ ingredients.js
@@ -1,1 +1,8 @@
+25 Easy Cake Recipes That Every Home Baker Should Master
 
+ngredients :
+
+. 2 tablespoons unsalted butter,softened . 1-1/2 cups sugar, divided . 3 eggs . 4 cups all-purpose flour . 1 tablespoon baking powder . 1/2 teaspoon salt . 3/4 cup 2% milk . Neutral oil, like canola or peanut, for deep-fat frying
+bake you cake serve it.
+
+Thank You ..
